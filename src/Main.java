6521cfee6fea2a027a855b40e500c562124fc604
mariorodriguez <mario.ra@students.salle.url.edu>
2022-03-31 16:24:09 +0200

    Log in + delete account calls into dataBase

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,22 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("First commit");
         UserSQLDAO sl = new UserSQLDAO();
-        sl.register("Mario","1234","mario@mario.com");
+        //sl.register("Mario","1234","mario@mario.com");
+        //sl.register("Guille","1234","guille@mario.com");
+        sl.register("Didac","1234","didac@mario.com");
+        //sl.register("Puchi","1234","puchi@mario.com");
+
+        boolean proba1 = sl.login("Mario","1234"); //prueba nombre buena
+        boolean proba2 = sl.login("mario@mario.com","1234"); //prueba correo buena
+        boolean proba3 = sl.login("guille@guille.com","1234"); // pureba correo mal
+        boolean proba4 = sl.login("didi","1234"); //prueba nombre mal
+        boolean proba5 = sl.login("Puchi","4321"); //prueba contra mal
+
+        System.out.println(proba1+"\t"+proba2+"\t"+proba3+"\t"+proba4+"\t"+proba5);
+
+        sl.deleteAccount("Didac");
+        sl.deleteAccount("Pepe");
+
         sl.mostrar();
     }
     
--- src/persistence/UserSQLDAO.java
+++ src/persistence/UserSQLDAO.java
@@ -2,23 +2,25 @@ package persistence;
 import business.json.*;
 import java.sql.*;
 
-public class UserSQLDAO  {
+
+public class UserSQLDAO {
     public UserSQLDAO() {
     }
 
     ConfigData data = JsonReader.llegeixJson();
-    public void register(String user, String pass,String mail) {
+
+    public void register(String user, String pass, String mail) {
         try {
             Connection connection = DriverManager.getConnection(data.getUrl(), data.getUser(), data.getPassword());
             Statement statement = connection.createStatement();
-            statement.execute("insert into player values('"+user+"','"+pass+"','"+mail+"',0,0,0)");
+            statement.execute("insert into player values('" + user + "','" + mail + "','" + pass + "',0,0,0)");
         } catch (SQLException e) {
             System.out.println("Connection failure.");
             e.printStackTrace();
         }
     }
 
-    public void mostrar(){
+    public void mostrar() {
         try {
             Connection connection = DriverManager.getConnection(data.getUrl(), data.getUser(), data.getPassword());
             Statement statement = connection.createStatement();
@@ -33,5 +35,46 @@ public class UserSQLDAO  {
         }
     }
 
+    public boolean login(String user,String pass) {
+        boolean correcte = false;
+        try {
+            Connection connection = DriverManager.getConnection(data.getUrl(), data.getUser(), data.getPassword());
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("Select * from player");
 
+            while ((resultSet.next())) {
+                if(resultSet.getString("name").equals(user) || resultSet.getString("mail").equals(user)){
+                    if(resultSet.getString("password").equals(pass)){
+                        correcte = true;
+                        return correcte;
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println("Connection failure.");
+            e.printStackTrace();
+        }
+        return correcte;
+    }
+
+    public void deleteAccount(String user){
+        try {
+            Connection connection = DriverManager.getConnection(data.getUrl(), data.getUser(), data.getPassword());
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("Select * from player");
+
+            while ((resultSet.next())) {
+                if(resultSet.getString("name").equals(user)){
+                    statement.execute("delete from player where name ='" + user + "'");
+                    System.out.println("The account has been successfully deleted");
+                    return;
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println("Connection failure.");
+            e.printStackTrace();
+        }
+        System.out.println("No s ha trobat el usuari");
+        return;
+    }
 }
