b69ea1fc4ebad468a6e353626dc773ec5f311ce1
mariorodriguez <mario.ra@students.salle.url.edu>
2022-03-31 13:33:24 +0200

    Database connection + first call and register

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,16 @@
-import business.json.ConfigData;
-import business.json.JsonReader;
+import persistence.UserSQLDAO;
 
-import javax.xml.namespace.QName;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("First commit");
-        ConfigData data;
-
-        data = JsonReader.llegeixJson(); //funcion para leer el Json
-        try (Connection connection = DriverManager.getConnection(data.getUrl(), data.getUser(), data.getPassword())) {
-
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM player");
-            while ((resultSet.next())){
-                System.out.println(resultSet.getString("name")+ "\t" + resultSet.getString("mail"));
-            }
-        }  catch (SQLException e) {
-            System.out.println("Connection failure.");
-            e.printStackTrace();
-        }
+        UserSQLDAO sl = new UserSQLDAO();
+        sl.register("Mario","1234","mario@mario.com");
+        sl.mostrar();
     }
     
 }
+
 
 
 
--- src/persistence/UserSQLDAO.java
+++ src/persistence/UserSQLDAO.java
@@ -1,4 +1,37 @@
 package persistence;
+import business.json.*;
+import java.sql.*;
 
-public class UserSQLDAO {
+public class UserSQLDAO  {
+    public UserSQLDAO() {
+    }
+
+    ConfigData data = JsonReader.llegeixJson();
+    public void register(String user, String pass,String mail) {
+        try {
+            Connection connection = DriverManager.getConnection(data.getUrl(), data.getUser(), data.getPassword());
+            Statement statement = connection.createStatement();
+            statement.execute("insert into player values('"+user+"','"+pass+"','"+mail+"',0,0,0)");
+        } catch (SQLException e) {
+            System.out.println("Connection failure.");
+            e.printStackTrace();
+        }
+    }
+
+    public void mostrar(){
+        try {
+            Connection connection = DriverManager.getConnection(data.getUrl(), data.getUser(), data.getPassword());
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("Select * from player");
+
+            while ((resultSet.next())) {
+                System.out.println(resultSet.getString("name") + "\t" + resultSet.getString("mail") + "\t" + resultSet.getString("password"));
+            }
+        } catch (SQLException e) {
+            System.out.println("Connection failure.");
+            e.printStackTrace();
+        }
+    }
+
+
 }
