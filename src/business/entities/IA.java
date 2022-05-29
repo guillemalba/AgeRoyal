e87131cca6c1dfbc860290c2f62ab9638fb2fb76
Guillem Alba <guillemalba@hotmail.com>
2022-05-29 11:33:27 +0200

    more comments

--- src/business/entities/IA.java
+++ src/business/entities/IA.java
@@ -15,7 +15,12 @@ public class IA implements Runnable{
     private GameManager gameManager;
     private int gameTime;
 
-    // todo luego hago
+    /**
+     * Constructor de la IA con el manager del juego y el timer de la partida
+     *
+     * @param gameManager del juego
+     * @param gameTime de la partida
+     */
     public IA(GameManager gameManager, int gameTime) {
         this.gameManager = gameManager;
         this.gameTime = gameTime;
@@ -47,6 +52,9 @@ public class IA implements Runnable{
         }
     }
 
+    /**
+     * Metodo que intenta invocar una tropa ofensiva en el mapa
+     */
     public void invokeOfensiveTroop() {
         int maxX = 7;
         int maxY = 14;
@@ -78,6 +86,9 @@ public class IA implements Runnable{
 
     }
 
+    /**
+     * Metodo que intenta invocar una tropa defensiva en el mapa
+     */
     public void invokeDefensiveTroop() {
         int maxX = 7;
         int maxY = 14;
@@ -115,6 +126,11 @@ public class IA implements Runnable{
 
     }
 
+    /**
+     * Metodo que encuentra la tropa del usuario que este mas cerca a la base de la IA
+     *
+     * @return posicion x (vertical) de la tropa, devuelve -1 si no ha encontrado tropa
+     */
     public int findNearestTroop() {
 
         int maxX = 7;
@@ -134,30 +150,65 @@ public class IA implements Runnable{
         return -1;
     }
 
+    /**
+     * Getter para devolver el dinero
+     *
+     * @return el dinero
+     */
     public int getMoney() {
         return money;
     }
 
+    /**
+     * Setter para actualizar el dinero
+     *
+     * @param money a actualizar
+     */
     public void setMoney(int money) {
         this.money = money;
     }
 
+    /**
+     * Getter para devolver el numero de tropas vivas que hay en el mapa
+     *
+     * @return numero de tropas vivas
+     */
     public int getNumTroopsAlive() {
         return numTroopsAlive;
     }
 
+    /**
+     * Setter para actualizar el numero de tropas vivas en el mapa
+     *
+     * @param numTroopsAlive tropas vivas
+     */
     public void setNumTroopsAlive(int numTroopsAlive) {
         this.numTroopsAlive = numTroopsAlive;
     }
 
+    /**
+     * Getter para devolver la vida de la base
+     *
+     * @return vida
+     */
     public int getLifes() {
         return lifes;
     }
 
+    /**
+     * Setter para actualizar la vida
+     *
+     * @param lifes vida a actualizar
+     */
     public void setLifes(int lifes) {
         this.lifes = lifes;
     }
 
+    /**
+     * Setter para detener el thread
+     *
+     * @param stop
+     */
     public void setStop(boolean stop) {
         this.stop = stop;
     }
